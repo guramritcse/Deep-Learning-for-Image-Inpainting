8ff98090bd8fdf8c81d82bbe2d4c05186f81bde5
nikbiradar <210050035@iitb.ac.in>
2023-11-29 13:54:27 +0530

    Added Contextual Attention Model

---
+++ Contextual-Attention/models.py
@@ -0,0 +1,424 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils import spectral_norm as spectral_norm_fn
+from torch.nn.utils import weight_norm as weight_norm_fn
+from ..utils.tools import extract_image_patches, flow_to_image,reduce_mean, reduce_sum, same_padding
+
+"""
+Generator Network.
+"""
+class Generator(nn.Module):
+    def __init__(self, config, use_cuda, device_ids):
+        super(Generator, self).__init__()
+        self.coarse_generator = CoarseGenerator(config['input_dim'], config['ngf'], use_cuda, device_ids)
+        self.fine_generator = FineGenerator(config['input_dim'], config['ngf'], use_cuda, device_ids)
+
+    def forward(self, img, mask):
+        coarse_img = self.coarse_generator(img, mask)
+        fine_img, offset_flow = self.fine_generator(img, coarse_img, mask)
+        return coarse_img, fine_img, offset_flow
+
+"""
+Coarse Generator Network.
+"""
+class CoarseGenerator(nn.Module):
+    def __init__(self, input_dim, cnum, use_cuda=True, device_ids=None):
+        super(CoarseGenerator, self).__init__()
+        self.use_cuda = use_cuda
+        self.device_ids = device_ids
+
+        self.conv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2)
+        self.conv2_downsample = gen_conv(cnum, cnum*2, 3, 2, 1)
+        self.conv3 = gen_conv(cnum*2, cnum*2, 3, 1, 1)
+        self.conv4_downsample = gen_conv(cnum*2, cnum*4, 3, 2, 1)
+        self.conv5 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv7_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 2, rate=2)
+        self.conv8_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 4, rate=4)
+        self.conv9_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 8, rate=8)
+        self.conv10_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 16, rate=16)
+        self.conv11 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv12 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv13 = gen_conv(cnum*4, cnum*2, 3, 1, 1)
+        self.conv14 = gen_conv(cnum*2, cnum*2, 3, 1, 1)
+        self.conv15 = gen_conv(cnum*2, cnum, 3, 1, 1)
+        self.conv16 = gen_conv(cnum, cnum//2, 3, 1, 1)
+        self.conv17 = gen_conv(cnum//2, input_dim, 3, 1, 1, activation='none')
+
+    def forward(self, img, mask):
+        ones = torch.ones(img.size(0), 1, img.size(2), img.size(3))
+        if self.use_cuda:
+            ones = ones.cuda()
+            mask = mask.cuda()
+
+        img = self.conv1(torch.cat([img, ones, mask], dim=1))
+        img = self.conv2_downsample(img)
+        img = self.conv3(img)
+        img = self.conv4_downsample(img)
+        img = self.conv5(img)
+        img = self.conv6(img)
+        img = self.conv7_atrous(img)
+        img = self.conv8_atrous(img)
+        img = self.conv9_atrous(img)
+        img = self.conv10_atrous(img)
+        img = self.conv11(img)
+        img = self.conv12(img)
+        img = F.interpolate(img, scale_factor=2, mode='nearest')
+        img = self.conv13(img)
+        img = self.conv14(img)
+        img = F.interpolate(img, scale_factor=2, mode='nearest')
+        img = self.conv15(img)
+        img = self.conv16(img)
+        img = self.conv17(img)
+        img_coarse = torch.clamp(img, -1., 1.)
+
+        return img_coarse
+
+"""
+Fine Generator Network.
+"""
+class FineGenerator(nn.Module):
+    def __init__(self, input_dim, cnum, use_cuda=True, device_ids=None):
+        super(FineGenerator, self).__init__()
+        self.use_cuda = use_cuda
+        self.device_ids = device_ids
+
+        self.conv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2)
+        self.conv2_downsample = gen_conv(cnum, cnum, 3, 2, 1)
+        self.conv3 = gen_conv(cnum, cnum*2, 3, 1, 1)
+        self.conv4_downsample = gen_conv(cnum*2, cnum*2, 3, 2, 1)
+        self.conv5 = gen_conv(cnum*2, cnum*4, 3, 1, 1)
+        self.conv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv7_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 2, rate=2)
+        self.conv8_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 4, rate=4)
+        self.conv9_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 8, rate=8)
+        self.conv10_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 16, rate=16)
+        self.pmconv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2)
+        self.pmconv2_downsample = gen_conv(cnum, cnum, 3, 2, 1)
+        self.pmconv3 = gen_conv(cnum, cnum*2, 3, 1, 1)
+        self.pmconv4_downsample = gen_conv(cnum*2, cnum*4, 3, 2, 1)
+        self.pmconv5 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.pmconv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1, activation='relu')
+        self.contextul_attention = ContextualAttention(ksize=3, stride=1, rate=2, fuse_k=3, softmax_scale=10,
+                                                       fuse=True, use_cuda=self.use_cuda, device_ids=self.device_ids)
+        self.pmconv9 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.pmconv10 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.allconv11 = gen_conv(cnum*8, cnum*4, 3, 1, 1)
+        self.allconv12 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.allconv13 = gen_conv(cnum*4, cnum*2, 3, 1, 1)
+        self.allconv14 = gen_conv(cnum*2, cnum*2, 3, 1, 1)
+        self.allconv15 = gen_conv(cnum*2, cnum, 3, 1, 1)
+        self.allconv16 = gen_conv(cnum, cnum//2, 3, 1, 1)
+        self.allconv17 = gen_conv(cnum//2, input_dim, 3, 1, 1, activation='none')
+
+    def forward(self, img, img_coarse, mask):
+        img_inpaint = img_coarse * mask + img * (1. - mask)
+        ones = torch.ones(img.size(0), 1, img.size(2), img.size(3))
+        if self.use_cuda:
+            ones = ones.cuda()
+            mask = mask.cuda()
+        xnow = torch.cat([img_inpaint, ones, mask], dim=1)
+        x = self.conv1(xnow)
+        x = self.conv2_downsample(x)
+        x = self.conv3(x)
+        x = self.conv4_downsample(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7_atrous(x)
+        x = self.conv8_atrous(x)
+        x = self.conv9_atrous(x)
+        x = self.conv10_atrous(x)
+        x_hallu = x
+        x = self.pmconv1(xnow)
+        x = self.pmconv2_downsample(x)
+        x = self.pmconv3(x)
+        x = self.pmconv4_downsample(x)
+        x = self.pmconv5(x)
+        x = self.pmconv6(x)
+        x, offset_flow = self.contextul_attention(x, x, mask)
+        x = self.pmconv9(x)
+        x = self.pmconv10(x)
+        pm = x
+        x = torch.cat([x_hallu, pm], dim=1)
+        x = self.allconv11(x)
+        x = self.allconv12(x)
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.allconv13(x)
+        x = self.allconv14(x)
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.allconv15(x)
+        x = self.allconv16(x)
+        x = self.allconv17(x)
+        img_fine = torch.clamp(x, -1., 1.)
+
+        return img_fine, offset_flow
+
+
+"""
+Contextual Attention Model .
+"""
+class ContextualAttention(nn.Module):
+    def __init__(self, ksize=3, stride=1, rate=1, fuse_k=3, softmax_scale=10,
+                 fuse=False, use_cuda=False, device_ids=None):
+        super(ContextualAttention, self).__init__()
+        self.ksize = ksize
+        self.stride = stride
+        self.rate = rate
+        self.fuse_k = fuse_k
+        self.softmax_scale = softmax_scale
+        self.fuse = fuse
+        self.use_cuda = use_cuda
+        self.device_ids = device_ids
+
+    def forward(self, f, b, mask=None):
+        raw_int_fs = list(f.size())   
+        raw_int_bs = list(b.size())   
+        kernel = 2 * self.rate
+        raw_w = extract_image_patches(b, ksizes=[kernel, kernel],
+                                      strides=[self.rate*self.stride,
+                                               self.rate*self.stride],
+                                      rates=[1, 1],
+                                      padding='same')
+        raw_w = raw_w.view(raw_int_bs[0], raw_int_bs[1], kernel, kernel, -1)
+        raw_w = raw_w.permute(0, 4, 1, 2, 3)    
+        raw_w_groups = torch.split(raw_w, 1, dim=0)
+
+        f = F.interpolate(f, scale_factor=1./self.rate, mode='nearest')
+        b = F.interpolate(b, scale_factor=1./self.rate, mode='nearest')
+        int_fs = list(f.size())     # b*c*h*w
+        int_bs = list(b.size())
+        f_groups = torch.split(f, 1, dim=0)  
+        w = extract_image_patches(b, ksizes=[self.ksize, self.ksize],
+                                  strides=[self.stride, self.stride],
+                                  rates=[1, 1],
+                                  padding='same')
+        w = w.view(int_bs[0], int_bs[1], self.ksize, self.ksize, -1)
+        w = w.permute(0, 4, 1, 2, 3)    
+        w_groups = torch.split(w, 1, dim=0)
+
+        if mask is None:
+            mask = torch.zeros([int_bs[0], 1, int_bs[2], int_bs[3]])
+            if self.use_cuda:
+                mask = mask.cuda()
+        else:
+            mask = F.interpolate(mask, scale_factor=1./(4*self.rate), mode='nearest')
+        int_ms = list(mask.size())
+        m = extract_image_patches(mask, ksizes=[self.ksize, self.ksize],
+                                  strides=[self.stride, self.stride],
+                                  rates=[1, 1],
+                                  padding='same')
+        m = m.view(int_ms[0], int_ms[1], self.ksize, self.ksize, -1)
+        m = m.permute(0, 4, 1, 2, 3)    
+        m = m[0]   
+        mm = (reduce_mean(m, axis=[1, 2, 3], keepdim=True)==0.).to(torch.float32)
+        mm = mm.permute(1, 0, 2, 3) 
+
+        y = []
+        offsets = []
+        k = self.fuse_k
+        scale = self.softmax_scale    
+        fuse_weight = torch.eye(k).view(1, 1, k, k)  
+        if self.use_cuda:
+            fuse_weight = fuse_weight.cuda()
+
+        for xi, wi, raw_wi in zip(f_groups, w_groups, raw_w_groups):
+            escape_NaN = torch.FloatTensor([1e-4])
+            if self.use_cuda:
+                escape_NaN = escape_NaN.cuda()
+            wi = wi[0]  
+            max_wi = torch.sqrt(reduce_sum(torch.pow(wi, 2) + escape_NaN, axis=[1, 2, 3], keepdim=True))
+            wi_normed = wi / max_wi
+            xi = same_padding(xi, [self.ksize, self.ksize], [1, 1], [1, 1])  
+            yi = F.conv2d(xi, wi_normed, stride=1)   
+            if self.fuse:
+                yi = yi.view(1, 1, int_bs[2]*int_bs[3], int_fs[2]*int_fs[3])  
+                yi = same_padding(yi, [k, k], [1, 1], [1, 1])
+                yi = F.conv2d(yi, fuse_weight, stride=1)  
+                yi = yi.contiguous().view(1, int_bs[2], int_bs[3], int_fs[2], int_fs[3])  
+                yi = yi.permute(0, 2, 1, 4, 3)
+                yi = yi.contiguous().view(1, 1, int_bs[2]*int_bs[3], int_fs[2]*int_fs[3])
+                yi = same_padding(yi, [k, k], [1, 1], [1, 1])
+                yi = F.conv2d(yi, fuse_weight, stride=1)
+                yi = yi.contiguous().view(1, int_bs[3], int_bs[2], int_fs[3], int_fs[2])
+                yi = yi.permute(0, 2, 1, 4, 3).contiguous()
+            yi = yi.view(1, int_bs[2] * int_bs[3], int_fs[2], int_fs[3])  
+            yi = yi * mm
+            yi = F.softmax(yi*scale, dim=1)
+            yi = yi * mm  
+
+            offset = torch.argmax(yi, dim=1, keepdim=True) 
+
+            if int_bs != int_fs:
+                times = float(int_fs[2] * int_fs[3]) / float(int_bs[2] * int_bs[3])
+                offset = ((offset + 1).float() * times - 1).to(torch.int64)
+            offset = torch.cat([torch.div(offset, int_fs[3], rounding_mode='trunc'), offset % int_fs[3]], dim=1)
+
+            wi_center = raw_wi[0]
+            yi = F.conv_transpose2d(yi, wi_center, stride=self.rate, padding=1) / 4. 
+            y.append(yi)
+            offsets.append(offset)
+
+        y = torch.cat(y, dim=0)  
+        y.contiguous().view(raw_int_fs)
+
+        offsets = torch.cat(offsets, dim=0)
+        offsets = offsets.view(int_fs[0], 2, *int_fs[2:])
+
+        h_add = torch.arange(int_fs[2]).view([1, 1, int_fs[2], 1]).expand(int_fs[0], -1, -1, int_fs[3])
+        w_add = torch.arange(int_fs[3]).view([1, 1, 1, int_fs[3]]).expand(int_fs[0], -1, int_fs[2], -1)
+        ref_coordinate = torch.cat([h_add, w_add], dim=1)
+        if self.use_cuda:
+            ref_coordinate = ref_coordinate.cuda()
+
+        offsets = offsets - ref_coordinate
+
+        flow = torch.from_numpy(flow_to_image(offsets.permute(0, 2, 3, 1).cpu().data.numpy())) / 255.
+        flow = flow.permute(0, 3, 1, 2)
+        if self.use_cuda:
+            flow = flow.cuda()
+
+        if self.rate != 1:
+            flow = F.interpolate(flow, scale_factor=self.rate*4, mode='nearest')
+
+        return y, flow
+
+"""
+Local Discriminator Network.
+"""
+class LocalDis(nn.Module):
+    def __init__(self, config, use_cuda=True, device_ids=None):
+        super(LocalDis, self).__init__()
+        self.input_dim = config['input_dim']
+        self.cnum = config['ndf']
+        self.use_cuda = use_cuda
+        self.device_ids = device_ids
+        self.dis_conv_module = DisConvModule(self.input_dim, self.cnum)
+        self.linear = nn.Linear(self.cnum*4*8*8, 1)
+
+    def forward(self, img):
+        img = self.dis_conv_module(img)
+        img = img.view(img.size()[0], -1)
+        img = self.linear(img)
+
+        return img
+
+
+"""
+Global Discriminator Network.
+"""
+class GlobalDis(nn.Module):
+    def __init__(self, config, use_cuda=True, device_ids=None):
+        super(GlobalDis, self).__init__()
+        self.input_dim = config['input_dim']
+        self.cnum = config['ndf']
+        self.use_cuda = use_cuda
+        self.device_ids = device_ids
+        self.dis_conv_module = DisConvModule(self.input_dim, self.cnum)
+        self.linear = nn.Linear(self.cnum*4*16*16, 1)
+
+    def forward(self, img):
+        img = self.dis_conv_module(img)
+        img = img.view(img.size()[0], -1)
+        img = self.linear(img)
+
+        return img
+
+"""
+Discriminator Convolution Module.
+"""
+class DisConvModule(nn.Module):
+    def __init__(self, input_dim, cnum, use_cuda=True, device_ids=None):
+        super(DisConvModule, self).__init__()
+        self.use_cuda = use_cuda
+        self.device_ids = device_ids
+
+        self.conv1 = dis_conv(input_dim, cnum, 5, 2, 2)
+        self.conv2 = dis_conv(cnum, cnum*2, 5, 2, 2)
+        self.conv3 = dis_conv(cnum*2, cnum*4, 5, 2, 2)
+        self.conv4 = dis_conv(cnum*4, cnum*4, 5, 2, 2)
+
+    def forward(self, img):
+        img = self.conv1(img)
+        img = self.conv2(img)
+        img = self.conv3(img)
+        img = self.conv4(img)
+
+        return img
+
+
+def gen_conv(input_dim, output_dim, kernel_size=3, stride=1, padding=0, rate=1,
+             activation='elu'):
+    return Conv2dBlock(input_dim, output_dim, kernel_size, stride,
+                       conv_padding=padding, dilation=rate,
+                       activation=activation)
+
+
+def dis_conv(input_dim, output_dim, kernel_size=5, stride=2, padding=0, rate=1,
+             activation='lrelu'):
+    return Conv2dBlock(input_dim, output_dim, kernel_size, stride,
+                       conv_padding=padding, dilation=rate,
+                       activation=activation)
+
+"""
+Convolution Block.
+"""
+class Conv2dBlock(nn.Module):
+    def __init__(self, input_dim, output_dim, kernel_size, stride, padding=0,
+                 conv_padding=0, dilation=1, weight_norm='none', norm='none',
+                 activation='relu', pad_type='zero', transpose=False):
+        super(Conv2dBlock, self).__init__()
+        self.use_bias = True
+        if pad_type == 'zero':
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            self.pad = None
+
+    
+        norm_dim = output_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm2d(norm_dim)
+        else:
+            self.norm = None
+
+        if weight_norm == 'sn':
+            self.weight_norm = spectral_norm_fn
+        elif weight_norm == 'wn':
+            self.weight_norm = weight_norm_fn
+        elif weight_norm == 'none':
+            self.weight_norm = None
+
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'elu':
+            self.activation = nn.ELU(inplace=True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        else:
+            self.activation = None
+
+        if transpose:
+            self.conv = nn.ConvTranspose2d(input_dim, output_dim,
+                                           kernel_size, stride,
+                                           padding=conv_padding,
+                                           output_padding=conv_padding,
+                                           dilation=dilation,
+                                           bias=self.use_bias)
+        else:
+            self.conv = nn.Conv2d(input_dim, output_dim, kernel_size, stride,
+                                  padding=conv_padding, dilation=dilation,
+                                  bias=self.use_bias)
+
+        if self.weight_norm:
+            self.conv = self.weight_norm(self.conv)
+
+    def forward(self, img):
+        if self.pad:
+            img = self.conv(self.pad(img))
+        else:
+            img = self.conv(img)
+        if self.norm:
+            img = self.norm(img)
+        if self.activation:
+            img = self.activation(img)
+        return img
---
+++ Contextual-Attention/test_single.py
@@ -0,0 +1,97 @@
+import os
+import random
+from argparse import ArgumentParser
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from models import Generator
+from ..utils.tools import get_config, image_loader, normalize, get_model_list
+
+# Parse arguments
+parser = ArgumentParser()
+parser.add_argument('--config', type=str, default='configs/test.yaml')
+parser.add_argument('--input', type=str,default="")
+parser.add_argument('--mask', type=str, default='')
+parser.add_argument('--output', type=str, default='output.png')
+parser.add_argument('--CA_map', type=str, default='')
+parser.add_argument('--checkpoint_path', type=str, default='')
+parser.add_argument('--test_iter', type=int, default=0)
+args = parser.parse_args()
+
+# Load experiment setting
+config = get_config(args.config)
+
+# Define device and configure CUDA
+cuda = config['cuda']
+device_ids = config['gpu_ids']
+if cuda:
+    os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(i) for i in device_ids)
+    device_ids = list(range(len(device_ids)))
+    config['gpu_ids'] = device_ids
+    cudnn.benchmark = True
+
+# Set random seed for reproducibility
+seed = 42
+random.seed(seed)
+torch.manual_seed(seed)
+if cuda:
+    torch.cuda.manual_seed_all(seed)
+
+"""
+Test the model on test set.
+"""
+with torch.no_grad():
+    data = image_loader(args.input)
+    data = transforms.Resize(config['image_shape'][:-1])(data)
+    data = transforms.CenterCrop(config['image_shape'][:-1])(data)
+    data = transforms.ToTensor()(data)
+    data = normalize(data)
+
+    mask = image_loader(args.mask)
+    mask = transforms.Resize(config['image_shape'][:-1])(mask)
+    mask = transforms.CenterCrop(config['image_shape'][:-1])(mask)
+    mask = transforms.ToTensor()(mask)[0].unsqueeze(dim=0)
+
+    data = data * (1. - mask)
+    data = data.unsqueeze(dim=0)
+    mask = mask.unsqueeze(dim=0)
+
+    # Define the checkpoint path
+    if not args.checkpoint_path:
+        checkpoint_path = os.path.join('checkpoints',
+                                        config['dataset_name'],
+                                        config['mask_type'] + '_' + config['expname'])
+    else:
+        checkpoint_path = args.checkpoint_path
+
+    # Define the model
+    netG = Generator(config['netG'], cuda, device_ids)
+    
+    # Load the trained model
+    last_model_name = get_model_list(checkpoint_path, "gen", iteration=args.test_iter)
+    netG.load_state_dict(torch.load(last_model_name))
+    model_iteration = int(last_model_name[-11:-3])
+    print(f"Loaded model from {checkpoint_path} at iteration {model_iteration}")
+
+    if cuda:
+        netG = nn.parallel.DataParallel(netG, device_ids=device_ids)
+        data = data.cuda()
+        mask = mask.cuda()
+        netG = netG.to("cuda")
+
+    # Get the inpainted result
+    _, patch_recon, offset_flow = netG(data, mask)
+    recon = patch_recon * mask + data * (1. - mask)
+
+    # Save the result
+    vutils.save_image(recon, args.output, padding=0, normalize=True)
+    print("Saved the inpainted result to {}".format(args.output))
+
+    # Save the Contextual Attention map
+    if args.CA_map:
+        vutils.save_image(offset_flow, args.CA_map, padding=0, normalize=True)
+        print("Saved offset flow to {}".format(args.CA_map))
+
+
